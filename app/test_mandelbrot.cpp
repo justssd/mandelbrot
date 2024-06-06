dddbe06cd957e98a9e760ee17e8653b1d3fd7220
Srinivass <srinivass@uvic.ca>
2024-06-06 09:22:29 -0700

    Implement point_to_complex
    Implement and test point_to_complex.

--- app/test_mandelbrot.cpp
+++ app/test_mandelbrot.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#include <cassert>
 #include "ra/mandelbrot.hpp"
 
+constexpr void test_point_to_complex() {
+    // auto invalid_WH_z = ra::fractal::point_to_complex(0, 0, 1, 1);
+    // auto point_outside_domain_z = ra::fractal::point_to_complex(3, 4, 4, 4);
+    auto z = ra::fractal::point_to_complex(10, 14, 15, 15);
+    assert(std::abs(z.real - (-0.0285711428)) < 1e-6);
+    assert(std::abs(z.imaginary - (-1.1)) < 1e-6);
+}
+
 int main() {
+    static_assert((test_point_to_complex(), true));
 // Force the image (in PNM format) to be computed at compile time.
     constexpr auto s = ra::fractal::mandelbrot<256, 256>;
     //constexpr ra::cexpr::cexpr_string<128> s("P1 8 4\n10000001\n01000010\n00100100\n00011000");
--- include/ra/mandelbrot.hpp
+++ include/ra/mandelbrot.hpp
@@ -37,6 +37,20 @@ public:
 template <std::size_t W, std::size_t H>
 constexpr ra::cexpr::cexpr_string<(W + 1) * H + 45>  mandelbrot;
 
+constexpr cexpr_complex point_to_complex(std::size_t k, std::size_t l, std::size_t W, std::size_t H) {
+    assert(W > 1 && H > 1 && "W and H cannot be < 2");
+    assert(k < W && l < H && "point outside domain");
+
+    constexpr double a_0 =  -1.6;
+    constexpr double a_1 =  -1.1;
+    constexpr double b_0 =  0.6;
+    constexpr double b_1 =  1.1;
+
+    double real = a_0 + k * (b_0 - a_0) / (W - 1);
+    double imaginary = a_1 + (H - 1 - l) * (b_1 - a_1) / (H - 1);
+    return cexpr_complex(real, imaginary);
+}
+
 }
 
 #endif // _CEXPR_MANDELBROT_
